b23ed4def39ad675ad26beb285484985b60f3b05
Sarah Pak <52671374+sarahspak@users.noreply.github.com>
2023-11-30 15:41:14 -0500

    adding more stuff

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # test-openai-imagegen
+
+
+ensure you have python installed:
+`which python`
+
+if you don't have python, install homebrew first: 
+`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"`
+
+then install python:
+`brew install python`
+
+create a virtualenv:
+`python -m venv venv`
+
+then activate it:
+`source venv/bin/activate`
+
+check you're in the right venv by running
+`which python`
+
+it should return something like: 
+`/Users/sarahpak/repos/test-openai-imagegen/venv/bin/python`
+
+
+## packages
+install openai package
+`pip install --upgrade openai`
+
+then pip install dotenv
+`pip install python-dotenv requests Pillow`
+
+
+# to run
+`python generate_images.py`
--- generate_images.py
+++ generate_images.py
@@ -1,13 +1,41 @@
 from openai import OpenAI
+from dotenv import load_dotenv, find_dotenv
+import os
+from PIL import Image
+import requests
+from datetime import datetime
 
+load_dotenv(find_dotenv())
+
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+# set environment variable OPENAI_API_KEY to your key
+os.environ["OPENAI_API_KEY"] = openai_api_key
 client = OpenAI()
 
 response = client.images.generate(
     model="dall-e-3",
-    prompt="a white siamese cat",
+    prompt="Image of organic bacterial growth texture, that tiles horizontally and fades into a small dot pattern at the top and bottom",
     size="1024x1024",
-    quality="standard",
+    quality="hd",  # one of hd or standard
+    style="vivid",
     n=1,
 )
 
 image_url = response.data[0].url
+print(image_url)
+# open and download the image_url
+data = requests.get(image_url).content
+
+# Opening a new file named img with extension .jpg
+# This file would store the data of the image file
+current_dt = datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f")
+f = open(f"{current_dt}_image.jpg", "wb")
+
+# Storing the image data inside the data variable to the file
+f.write(data)
+f.close()
+
+# Opening the saved image and displaying it
+img = Image.open(f"{current_dt}_image.jpg")
+img.show()
